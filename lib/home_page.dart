29b91a0c4389e7115c4d2de054fd67a67c8ac56b
AVISH CHOUDHARY <choudharyavish0@gmail.com>
2026-07-14 18:48:05 +0530

    added basic elements and tags

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  final List<String> filter = const ['Samsung', 'Google', 'Apple', 'Xiaomi'];
 
   @override
   Widget build(BuildContext context) {
@@ -9,20 +10,20 @@ class HomePage extends StatelessWidget {
       borderSide: BorderSide(color: Color.fromRGBO(225, 225, 225, 1)),
       borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
     );
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
             Row(
               children: [
-                Padding(
+                const Padding(
                   padding: EdgeInsets.all(20.0),
                   child: Text(
                     "helloo\noooooo",
                     style: TextStyle(fontWeight: FontWeight(700), fontSize: 40),
                   ),
                 ),
-                Expanded(
+                const Expanded(
                   child: TextField(
                     decoration: InputDecoration(
                       hint: Text("Search here"),
@@ -34,6 +35,23 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  final label = filter[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                      vertical: 0,
+                    ),
+                    child: Chip(label: Text(label)),
+                  );
+                },
+                itemCount: filter.length,
+                scrollDirection: Axis.horizontal,
+              ),
             ),
           ],
         ),
